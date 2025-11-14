e70d31f6ab127f66aaf34b22efa48afed2e2c90e
Psychorian <psychoroleplays@gmail.com>
2025-11-14 02:26:58 -0600

    Initial commit

---
+++ src/main/java/net/psychorian/greekmod/GreekMod.java
@@ -0,0 +1,16 @@
+package net.psychorian.greekmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GreekMod implements ModInitializer {
+	public static final String MOD_ID = "greekmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/psychorian/greekmod/GreekModClient.java
@@ -0,0 +1,10 @@
+package net.psychorian.greekmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GreekModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
